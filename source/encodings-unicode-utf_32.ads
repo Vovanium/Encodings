6b22e4321a78441d9036f697361cc32e3b7fe006
Vovanium <vovanium@protonmail.ch>
2019-08-08 23:40:19 +0300

    + unicode support started

---
+++ source/encodings-unicode-utf_32.ads
@@ -0,0 +1,3 @@
+package Encodings.Unicode.UTF_32 is
+	subtype UTF_32_String is Wide_Wide_String;
+end Encodings.Unicode.UTF_32;
---
+++ source/encodings-unicode.ads
@@ -0,0 +1,3 @@
+package Encodings.Unicode is
+	Code_Point_Last: constant Wide_Wide_Character := Wide_Wide_Character'Val(16#10FFFF#);
+end Encodings.Unicode;
